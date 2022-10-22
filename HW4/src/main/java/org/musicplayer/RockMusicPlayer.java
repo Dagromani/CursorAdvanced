8ed380822d93d35bd8c25c84626f2da4a563d03d
Dagromani <109410300+Dagromani@users.noreply.github.com>
2022-10-22 16:17:02 +0300

    Add files via upload

---
+++ HW4/src/main/java/org/musicplayer/RockMusicPlayer.java
@@ -0,0 +1,21 @@
+package org.musicplayer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RockMusicPlayer {
+    private List<Song> rockMusic = new ArrayList<>();
+
+    public RockMusicPlayer() {
+        Song LPPapercut = new Song("Papercut" , "Linkin Park");
+        Song TDGTimeOfDying = new Song("Time of Dying" , "Three Days Grace");
+        rockMusic.add(LPPapercut);
+        rockMusic.add(TDGTimeOfDying);
+    }
+
+    public void playRockMusic() {
+        for(int i = 0 ; i < rockMusic.size() ; i++) {
+            System.out.println("Playing: " + rockMusic.get(i));
+        }
+    }
+}
