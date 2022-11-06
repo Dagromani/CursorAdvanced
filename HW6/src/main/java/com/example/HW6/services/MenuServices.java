ff6852703f96833af0913019569665c0921d23e0
Dagromani <109410300+Dagromani@users.noreply.github.com>
2022-11-06 13:36:55 +0200

    Add files via upload

---
+++ HW6/src/main/java/com/example/HW6/services/MenuServices.java
@@ -0,0 +1,98 @@
+package com.example.HW6.services;
+
+import com.example.HW6.entity.User;
+import com.example.HW6.models.MenuModels;
+import com.example.HW6.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.Scanner;
+
+@Service
+public class MenuServices {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void showMenu() {
+        System.out.println(MenuModels.CHOOSE);
+        System.out.println(MenuModels.FIRST_OPTION);
+        System.out.println(MenuModels.SECOND_OPTION);
+        System.out.println(MenuModels.THIRD_OPTION);
+        System.out.println(MenuModels.FOURTH_OPTION);
+        System.out.println(MenuModels.FIFTH_OPTION);
+    }
+
+    public int makeDecision() {
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        return i;
+    }
+
+    public User settingUser() {
+        Scanner scanner = new Scanner(System.in);
+        Scanner scanner2 = new Scanner(System.in);
+        User newUser = new User();
+        System.out.println("Enter user first name.");
+        newUser.setFirstName(scanner.nextLine());
+        System.out.println("Enter user second name.");
+        newUser.setSecondName(scanner.nextLine());
+        System.out.println("Enter user age.");
+        newUser.setAge(scanner.nextInt());
+        System.out.println("Enter user gender.");
+        newUser.setGender(scanner2.nextLine());
+        return newUser;
+    }
+
+    public User settingUserWithId() {
+        Scanner scanner = new Scanner(System.in);
+        Scanner scanner2 = new Scanner(System.in);
+        Scanner scanner3 = new Scanner(System.in);
+        User newUser = new User();
+        System.out.println("Enter user id.");
+        newUser.setId(scanner3.nextLong());
+        System.out.println("Enter user first name.");
+        newUser.setFirstName(scanner.nextLine());
+        System.out.println("Enter user second name.");
+        newUser.setSecondName(scanner.nextLine());
+        System.out.println("Enter user age.");
+        newUser.setAge(scanner.nextInt());
+        System.out.println("Enter user gender.");
+        newUser.setGender(scanner2.nextLine());
+        return newUser;
+    }
+
+    public Long enterId() {
+        Scanner scanner = new Scanner(System.in);
+        Long i;
+        System.out.println("Enter user id:");
+        i = scanner.nextLong();
+        return i;
+    }
+
+    @Transactional
+    public void createUserOption() {
+        userRepository.save(settingUser());
+        System.out.println("User added!");
+    }
+
+    @Transactional
+    public void updateUserOption() {
+        userRepository.update(settingUserWithId());
+        System.out.println("User updated!");
+    }
+
+    @Transactional
+    public void deleteUserOption() {
+        userRepository.deleteUser(enterId());
+        System.out.println("User deleted!");
+    }
+
+    public void showConcreteUserOption() {
+        System.out.println(userRepository.findById(enterId()));
+    }
+
+    public void showAllUsersOption() {
+        System.out.println(userRepository.getAll());
+    }
+}
