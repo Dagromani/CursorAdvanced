edae79a54b4e9cdda76855f4a21f083de43b4d32
Dagromani <109410300+Dagromani@users.noreply.github.com>
2022-11-06 13:32:59 +0200

    Delete HW7 directory

--- HW7/src/main/java/com/example/HW7/responseStatusException/ShopNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.HW7.responseStatusException;
-
-public class ShopNotFoundException extends RuntimeException {
-    public ShopNotFoundException(String message) {
-        super(message);
-    }
-}
