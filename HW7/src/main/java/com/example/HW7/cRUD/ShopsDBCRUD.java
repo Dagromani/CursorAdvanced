a568b0eb7f271276c1f01be455cf7ba3e27e350f
Dagromani <109410300+Dagromani@users.noreply.github.com>
2022-11-06 13:32:24 +0200

    Add files via upload

---
+++ HW7/src/main/java/com/example/HW7/cRUD/ShopsDBCRUD.java
@@ -0,0 +1,31 @@
+package com.example.HW7.cRUD;
+
+import com.example.HW7.dB.ShopsDB;
+import com.example.HW7.models.Shop;
+
+import java.util.HashMap;
+
+public class ShopsDBCRUD {
+
+    private ShopsDB shopsDB = ShopsDB.getInstance();
+
+    public void addShop(Shop shop) {
+        shopsDB.getShopsDB().put(shop.getId(), shop);
+    }
+
+    public void removeShopWithID(Long id) {
+        shopsDB.getShopsDB().remove(id);
+    }
+
+    public HashMap<Long, Shop> getAllShops() {
+        return shopsDB.getShopsDB();
+    }
+
+    public Shop getShopWithID(Long id) {
+        return shopsDB.getShopsDB().get(id);
+    }
+
+    public void updateShop(Long id, Shop shop) {
+        shopsDB.getShopsDB().replace(id, new Shop(id, shop.getCity(), shop.getStreet(), shop.getName(), shop.getEmployeeQuantity(), shop.isWebSiteAvailability()));
+    }
+}
