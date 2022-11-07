38cabc3eae700838cec8f6eb595e9efc98373612
Dagromani <109410300+Dagromani@users.noreply.github.com>
2022-11-07 15:57:39 +0200

    Add files via upload

---
+++ HW8/src/main/java/com/example/HW8/Hw8Application.java
@@ -0,0 +1,18 @@
+package com.example.HW8;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.Duration;
+import java.time.temporal.Temporal;
+import java.util.Date;
+
+@SpringBootApplication
+public class Hw8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw8Application.class, args);
+    }
+
+}
---
+++ HW8/src/main/java/com/example/HW8/models/Product.java
@@ -0,0 +1,76 @@
+package com.example.HW8.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private float price;
+    private String description;
+    private String countryProducer;
+    private Date yearWhenProduced;
+    private Date expirationDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCountryProducer() {
+        return countryProducer;
+    }
+
+    public void setCountryProducer(String countryProducer) {
+        this.countryProducer = countryProducer;
+    }
+
+    public Date getYearWhenProduced() {
+        return yearWhenProduced;
+    }
+
+    public void setYearWhenProduced(Date yearWhenProduced) {
+        this.yearWhenProduced = yearWhenProduced;
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+}
