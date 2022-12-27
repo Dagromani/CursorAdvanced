f21b56116b0b7eff68732936e8599c3f3253fed0
Dagromani <109410300+Dagromani@users.noreply.github.com>
2022-12-27 14:28:11 +0200

    Delete HW10 directory

--- HW10/src/main/java/com/example/HW10/repository/ShopRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.HW10.repository;
-
-import com.example.HW10.entity.Shop;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShopRepository extends JpaRepository<Shop, Long> {
-}
